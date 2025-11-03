bcf2566c59b96e1b757d05a664d4c019a1df35ea
Tobias Koppers <tobias.koppers@googlemail.com>
2025-11-03 12:15:01 +0100

    Turbopack: only enable nested async availability in production
    This avoid the combinations of paths problem in dev since this creates a lot of output files in development (every chunk group has a register chunk group chunk in dev, and we don't have commons chunk optimization in dev)

--- turbopack/crates/turbopack-cli/src/build/mod.rs
+++ turbopack/crates/turbopack-cli/src/build/mod.rs
@@ -368,6 +368,7 @@ async fn build_internal(
                             },
                         )
                         .use_content_hashing(ContentHashing::Direct { length: 16 })
+                        .nested_async_availability(true)
                         .module_merging(scope_hoist);
                 }
             }
